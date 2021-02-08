6fb58d80138fc9183d659cad21d9c69341e81bc0
John Milner <johnmilnerapple@gmail.com>
2021-02-08 15:32:18 -0800

    forkify project commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '165a2c59-a2df-440d-99d6-077c08e56fd8';
+export const MODAL_CLOSE_SEC = 2.5;
